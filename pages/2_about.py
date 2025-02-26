c6022500a29dd1c6b74ac1006f945bd5c4999a26
kapellmeister84 <48524327+kapellmeister84@users.noreply.github.com>
2025-02-26 23:01:35 +0100

    Update 2_about.py

--- pages/2_about.py
+++ pages/2_about.py
@@ -1,4 +1,8 @@
 # pages/2_About.py
 import streamlit as st
+from utils import load_css
+
+load_css()
+
 st.title("About")
 st.markdown("Hier erzählst du deine Geschichte, Visionen und was dir sonst noch auf dem Herzen liegt. Ganz elegant und authentisch, wie du es magst!")
