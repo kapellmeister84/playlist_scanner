d018d060c3b2907a7b12442f32e94364672572ee
kapellmeister84 <48524327+kapellmeister84@users.noreply.github.com>
2025-02-26 23:14:36 +0100

    Update 2_about.py

--- pages/2_about.py
+++ pages/2_about.py
@@ -6,5 +6,7 @@ load_css()
 
 st.title("About")
 st.markdown("""
-I am a music producer and I know the struggle: many artists spend the night from Thursday to release Friday manually searching every playlist to check if their new song got placed. This app makes that process much easier. If you have any suggestions or if you'd like additional playlists to be added, feel free to email me at t.schroth@echolux.de.
+I’m a music producer, so I get it—every Thursday night turns into a hunt through playlists, hoping to spot your new release. This app takes that hassle away, giving you quick answers without the late-night scrolling. Got suggestions or missing playlists you’d like to see? Drop me a line at t.schroth@echolux.de—I’d love to hear from you!
+
+**Disclaimer:** By signing up, you agree to the collection, storage, and processing of your data for the purpose of providing this service. The app owner is not liable for any damages, losses, or issues that may arise from using this application. Use it at your own discretion.
 """)
