87264947a2f3322c96bb7bec3757044065fafa5d
kapellmeister84 <48524327+kapellmeister84@users.noreply.github.com>
2025-02-26 23:10:30 +0100

    Update 2_about.py

--- pages/2_about.py
+++ pages/2_about.py
@@ -5,7 +5,7 @@ from utils import load_css
 load_css()
 
 st.title("About")
-st.markdown("I’m a music producer and I know the struggle: many artists spend the night from Thursday to release Friday
+st.markdown("I am a music producer and I know the struggle: many artists spend the night from Thursday to release Friday
 manually searching every playlist to see if their new song got placed. 
 This app makes that process much easier. 
 If you have suggestions or would like additional playlists to be added, feel free to email me at t.schroth@echolux.de.
