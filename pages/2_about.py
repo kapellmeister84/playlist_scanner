b8a0b59f05c1c8f98f599bd2a3455f42879f079a
kapellmeister84 <48524327+kapellmeister84@users.noreply.github.com>
2025-02-26 22:40:45 +0100

    Create 2_about.py

---
+++ pages/2_about.py
@@ -0,0 +1,4 @@
+# pages/2_About.py
+import streamlit as st
+st.title("About")
+st.markdown("Hier erzählst du deine Geschichte, Visionen und was dir sonst noch auf dem Herzen liegt. Ganz elegant und authentisch, wie du es magst!")
