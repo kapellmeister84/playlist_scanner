58863e5b98f2c876e92e316270f96ddf770c2eda
kapellmeister84 <48524327+kapellmeister84@users.noreply.github.com>
2025-07-25 18:43:52 +0200

    Update playlist_scanner.py

--- playlist_scanner.py
+++ playlist_scanner.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import base64
 import asyncio
 import shutil
+import subprocess
 from pathlib import Path
 from playwright.async_api import async_playwright
 from fpdf import FPDF
